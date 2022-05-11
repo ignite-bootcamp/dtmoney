5c31e73d1bfbb50738c3b5766f2ecd899c400e48
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-11 12:04:47 -0300

    feat(components): creating modal structure and styling global modal classes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -11,8 +11,20 @@ export function NewTransactionsModal({
   onRequestClose
 }: NewTransactionsModalProps) {
   return (
-    <Modal onRequestClose={onRequestClose} isOpen={isOpen}>
-      <Container>hello</Container>
+    <Modal
+      onRequestClose={onRequestClose}
+      isOpen={isOpen}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
